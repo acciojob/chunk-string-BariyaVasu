9b6faaa08322188322584fd28512b66b67257f5f
Bariya Vishwas <bariyavasu46@gmail.com>
2025-06-20 13:50:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -8,6 +8,24 @@ function stringChop(str, size) {
 // Do not change the code below
 const str = prompt("Enter String.");
 
+function stringChop(str, size) {
+  const chunks = [];
+  size = Number(size); // Convert once at the start
+
+  if (isNaN(size) || size <= 0) {
+    return ["Invalid chunk size"];
+  }
+
+  for (let i = 0; i < str.length; i += size) {
+    chunks.push(str.slice(i, i + size));
+  }
+  return chunks;
+}
+
+// Do not change the code below
+const str = prompt("Enter String.");
+const size = prompt("Enter Chunk Size.");
+alert(stringChop(str, size).join(', '));
 
 
 const size = prompt("Enter Chunk Size.");
