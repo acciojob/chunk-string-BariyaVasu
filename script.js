65c91c6b2ed720ec6a6be360ef775284d3fb1576
Bariya Vishwas <bariyavasu46@gmail.com>
2025-06-26 23:44:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,32 +1,9 @@
-function stringChop(str, size) {
+function chunkString(str, chunkLength) {
   const chunks = [];
-  for (let i = 0; i < str.length; i += Number(size)) {
-    chunks.push(str.slice(i, i + Number(size)));
+
+  for (let i = 0; i < str.length; i += chunkLength) {
+    chunks.push(str.slice(i, i + chunkLength));
   }
+
   return chunks;
 }
-// Do not change the code below
-const str = prompt("Enter String.");
-
-function stringChop(str, size) {
-  const chunks = [];
-  size = Number(size); // Convert once at the start
-
-  if (isNaN(size) || size <= 0) {
-    return ["Invalid chunk size"];
-  }
-
-  for (let i = 0; i < str.length; i += size) {
-    chunks.push(str.slice(i, i + size));
-  }
-  return chunks;
-}
-
-// Do not change the code below
-const str = prompt("Enter String.");
-const size = prompt("Enter Chunk Size.");
-alert(stringChop(str, size).join(', '));
-
-
-const size = prompt("Enter Chunk Size.");
-alert(stringChop(str, size).join(','));
